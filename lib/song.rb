38bbbd7a10f9a4accb295c5f5de5de23493e86d5
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 16:08:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     end
     
      def self.find_by_name(name)
-      self.all.find {|person| person.name == name}
+      self.all.find {|x| x.name == name}
     end 
     
     def self.alphabetical(name)
