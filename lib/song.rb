1ec3fceb915a058a2d7382dd9c7e16a401ec15f8
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 16:39:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     end 
    
     
-    def self.new_fron_filename(filename)
+    def self.new_from_filename(filename)
       split_filename = filename.chomp(".mp3").split(" - ")
       song = Song.new
       song.name = split_filename[1]
