8d9d42104f049bdb5263a63cb2c0fcbb8f999518
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 16:39:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,10 +44,10 @@ class Song
     def self.alphabetical
         @@all.sort_by { |song| song.name}
     end 
-    
+   
     
     def self.new_fron_filename(filename)
-      split_filename = filename.chop.chop.chop.cop.split(" - ")
+      split_filename = filename.chomp(".mp3").split(" - ")
       song = Song.new
       song.name = split_filename[1]
       song.artist_name = split_filename[0]
