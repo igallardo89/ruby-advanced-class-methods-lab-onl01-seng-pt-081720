f50af8fdeb5d758007b71c045ee064fee10373ab
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-11 15:48:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,24 @@ class Song
   def self.all
     @@all
   end
-
   def save
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+   end 
+   
+   def self.new_by_name(n)
+     
+     song = self.new
+     song.name = n 
+     song.save
+     song
+   end 
+     
+     
 end
+
